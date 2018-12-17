95d6067540a252aa59114ef3e472acd56cd7813c
wajihullahbaig <wajihullahbaig@gmail.com>
2018-12-17 12:49:14 +0500

    Adding file for tests with Neural Networks
    PyTorch based neural network experiments

---
+++ mnist_nn.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 17 12:10:50 2018
+
+@author: wajih
+"""
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import scipy.io
+import numpy as np
+
+# Prepare Dataset
+# load data
+inputData = scipy.io.loadmat('/media/wajih/Disk1 500 GB/Onus/RnD/DataSet/handwritten/mnist/mnist_uint8')
+if inputData is not None:
+    keys = inputData.keys()
+    for key in keys:
+        if key == "train_x":
+            train_X = inputData[key].copy()
+            train_X = train_X/255.0
+        elif key == "train_y":
+            train_Y = inputData[key].copy()
+            train_Y = train_Y
+        elif key == "test_x":
+            test_X = inputData[key].copy()
+            test_X = test_X/255.0
+        elif key == "test_y":
+            test_Y = inputData[key].copy()
+            test_Y = test_Y            
+        
+# Reduce the one hot encoding
+train_Y = np.argmax(train_Y, axis=1)
+test_Y = np.argmax(test_Y,axis=1)
+# Lets view an entry 
+index = 77
+v = test_X[index,:].reshape(28,28)
+fig = plt.figure(figsize = (10,10))
+ax = fig.gca()
+plt.title("Original Image - 1D plot",fontsize=15)
+plt.plot(test_X[index,:])
+plt.show()
+
+fig = plt.figure(figsize = (10,10))
+ax = fig.gca()
+plt.title("Original Image",fontsize=10)
+plt.imshow(v)
+plt.show()
+# create feature and targets tensor for train set. As you remember we need variable to accumulate gradients. Therefore first we create tensor, then we will create variable
+featuresTrain = torch.from_numpy(train_X).type(torch.FloatTensor)
+targetsTrain = torch.from_numpy(train_Y).type(torch.LongTensor) # data type is long
+
+# create feature and targets tensor for test set.
+featuresTest = torch.from_numpy(test_X).type(torch.FloatTensor)
+targetsTest = torch.from_numpy(test_Y).type(torch.LongTensor) # data type is long
+
+# batch_size, epoch and iteration
+batch_size = 100
+n_iters = 10000
+num_epochs = n_iters / (len(train_X) / batch_size)
+num_epochs = int(num_epochs)
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(featuresTrain,targetsTrain)
+test = torch.utils.data.TensorDataset(featuresTest,targetsTest)
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size = batch_size, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = batch_size, shuffle = False)
+
+
+
+# Create ANN Model
+class ANNModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(ANNModel, self).__init__()
+        # Linear function 1: 784 --> 100
+        self.fc1 = nn.Linear(input_dim, hidden_dim) 
+        # Non-linearity 1
+        self.relu1 = nn.ReLU()
+        
+        # Linear function 2: 100 --> 100
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        # Non-linearity 2
+        self.tanh2 = nn.Tanh()
+        
+        # Linear function 3: 100 --> 100
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        # Non-linearity 3
+        self.elu3 = nn.ELU()
+        
+        # Linear function 4 (readout): 100 --> 10
+        self.fc4 = nn.Linear(hidden_dim, output_dim)  
+    
+    def forward(self, x):
+        # Linear function 1
+        out = self.fc1(x)
+        # Non-linearity 1
+        out = self.relu1(out)
+        
+        # Linear function 2
+        out = self.fc2(out)
+        # Non-linearity 2
+        out = self.tanh2(out)
+        
+        # Linear function 2
+        out = self.fc3(out)
+        # Non-linearity 2
+        out = self.elu3(out)
+        
+        # Linear function 4 (readout)
+        out = self.fc4(out)
+        return out
+
+# instantiate ANN
+input_dim = 784 
+hidden_dim = 99 #hidden layer dim is one of the hyper parameter and it should be chosen and tuned. For now I only say 150 there is no reason.
+output_dim = 10
+
+# Create ANN
+model = ANNModel(input_dim, hidden_dim, output_dim)
+
+# Cross Entropy Loss 
+error = nn.CrossEntropyLoss()
+
+# SGD Optimizer
+learning_rate = 0.2
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# ANN model training
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+
+        train = Variable(images.view(-1, 784))
+        labels = Variable(labels)
+        
+        # Clear gradients
+        optimizer.zero_grad()
+        
+        # Forward propagation
+        outputs = model(train)
+        
+        # Calculate softmax and ross entropy loss
+        loss = error(outputs, labels)
+        
+        # Calculating gradients
+        loss.backward()
+        
+        # Update parameters
+        optimizer.step()
+        
+        count += 1
+        
+        if count % 50 == 0:
+            # Calculate Accuracy         
+            correct = 0
+            total = 0
+            # Predict test dataset
+            for images, labels in test_loader:
+
+                test = Variable(images.view(-1, 784))
+                
+                # Forward propagation
+                outputs = model(test)
+                
+                # Get predictions from the maximum value
+                predicted = torch.max(outputs.data, 1)[1]
+                
+                # Total number of labels
+                total += len(labels)
+
+                # Total correct predictions
+                correct += (predicted == labels).sum()
+            
+            accuracy = 100 * correct / float(total)
+            
+            # store loss and iteration
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+            if count % 500 == 0:
+                # Print Loss
+                print('Iteration: {}  Loss: {}  Accuracy: {} %'.format(count, loss.item(), accuracy))
+
+
+# visualization loss 
+plt.plot(iteration_list,loss_list)
+plt.xlabel("Number of iteration")
+plt.ylabel("Loss")
+plt.title("ANN: Loss vs Number of iteration")
+plt.show()
+
+# visualization accuracy 
+plt.plot(iteration_list,accuracy_list,color = "red")
+plt.xlabel("Number of iteration")
+plt.ylabel("Accuracy")
+plt.title("ANN: Accuracy vs Number of iteration")
+plt.show()
+
+
+del model,error, optimizer
+
+test_x_gist = scipy.io.loadmat('//media/wajih/Disk1 500 GB/Onus/RnD/Codes/gistdescriptor/mnist_gists/test_x_gist')
+train_x_gist = scipy.io.loadmat('//media/wajih/Disk1 500 GB/Onus/RnD/Codes/gistdescriptor/mnist_gists/train_x_gist')
+
+
+keys = train_x_gist.keys()
+for key in keys:
+    if key == "train_x_gist":
+        train_X = train_x_gist[key].copy()
+keys = test_x_gist.keys()
+for key in keys:
+    if key == "test_x_gist":
+        test_X = test_x_gist[key].copy()
+
+# Lets view an entry 
+fig = plt.figure(figsize = (10,10))
+ax = fig.gca()
+plt.title("Gist plot",fontsize=15)
+plt.plot(test_X[index,:])
+plt.show()
+
+# create feature and targets tensor for train set. As you remember we need variable to accumulate gradients. Therefore first we create tensor, then we will create variable
+featuresTrain = torch.from_numpy(train_X).type(torch.FloatTensor)
+targetsTrain = torch.from_numpy(train_Y).type(torch.LongTensor) # data type is long
+
+# create feature and targets tensor for test set.
+featuresTest = torch.from_numpy(test_X).type(torch.FloatTensor)
+targetsTest = torch.from_numpy(test_Y).type(torch.LongTensor) # data type is long
+
+num_epochs = n_iters / (len(train_X) / batch_size)
+num_epochs = int(num_epochs)
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(featuresTrain,targetsTrain)
+test = torch.utils.data.TensorDataset(featuresTest,targetsTest)
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size = batch_size, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = batch_size, shuffle = False)
+
+
+# instantiate ANN
+input_dim = 224 
+
+# Create ANN
+model = ANNModel(input_dim, hidden_dim, output_dim)
+
+# Cross Entropy Loss 
+error = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# ANN model training
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+
+        train = Variable(images.view(-1, 224))
+        labels = Variable(labels)
+        
+        # Clear gradients
+        optimizer.zero_grad()
+        
+        # Forward propagation
+        outputs = model(train)
+        
+        # Calculate softmax and ross entropy loss
+        loss = error(outputs, labels)
+        
+        # Calculating gradients
+        loss.backward()
+        
+        # Update parameters
+        optimizer.step()
+        
+        count += 1
+        
+        if count % 50 == 0:
+            # Calculate Accuracy         
+            correct = 0
+            total = 0
+            # Predict test dataset
+            for images, labels in test_loader:
+
+                test = Variable(images.view(-1, 224))
+                
+                # Forward propagation
+                outputs = model(test)
+                
+                # Get predictions from the maximum value
+                predicted = torch.max(outputs.data, 1)[1]
+                
+                # Total number of labels
+                total += len(labels)
+
+                # Total correct predictions
+                correct += (predicted == labels).sum()
+            
+            accuracy = 100 * correct / float(total)
+            
+            # store loss and iteration
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+            if count % 500 == 0:
+                # Print Loss
+                print('Iteration: {}  Loss: {}  Accuracy: {} %'.format(count, loss.item(), accuracy))
+
+
+# visualization loss 
+plt.plot(iteration_list,loss_list)
+plt.xlabel("Number of iteration")
+plt.ylabel("Loss")
+plt.title("ANN: Loss vs Number of iteration")
+plt.show()
+
+# visualization accuracy 
+plt.plot(iteration_list,accuracy_list,color = "red")
+plt.xlabel("Number of iteration")
+plt.ylabel("Accuracy")
+plt.title("ANN: Accuracy vs Number of iteration")
+plt.show()
+
